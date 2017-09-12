2593007535d9461eb3f796dc1728c5c76d1008c6
lvxin <18730231789@163.com>
2017-09-12 18:54:38 +0800

    Rsa pubsky 修改

--- fenzhi/fenzhi/DataManger/LogDataMangerViewController.swift
+++ fenzhi/fenzhi/DataManger/LogDataMangerViewController.swift
@@ -15,10 +15,9 @@ class LogDataMangerViewController: FZRequestViewController {
 
     func login(phoneNum : String, paseWord : String, completion : @escaping (_ data : Any) ->(), failure : @escaping (_ error : Any)->()) {
         
-        let  pubkey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQK\nBgQCs/AKBw1GMHK+0JS6MP+6UJ2WM\nUU1yX4qKhkKvn1vzTQjzIEf1X92+4kH51wkrWVn\nWy8dObblMu6CERDs+lqzkmkp5\np3Yv9wMHnNhxwZYr+qMr73X5Pkn41Qsdbe+rx\nGO7dWSuhmcgY2EbZle6N8bXssZk\n8yK+gz6ReGr7k2lgHwIDAQAB";
-
+        let pubkey = "-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCs/AKBw1GMHK+0JS6MP+6UJ2WM\nUU1yX4qKhkKvn1vzTQjzIEf1X92+4kH51wkrWVnWy8dObblMu6CERDs+lqzkmkp5\np3Yv9wMHnNhxwZYr+qMr73X5Pkn41Qsdbe+rxGO7dWSuhmcgY2EbZle6N8bXssZk\n8yK+gz6ReGr7k2lgHwIDAQAB\n-----END PUBLIC KEY-----\n";
         
-        let  privkey = "MIICXAIBAAKBgQCs/AKBw1GMHK+0JS6MP+6UJ2WMUU1yX4qKhkKvn1vzTQjzIEf1X92+4kH51wkrWVnWy8dObblMu6CERDs+lqzkmkp5p3Yv9wMHnNhxwZYr+qMr73X5Pkn41Qsdbe+rxGO7dWSuhmcgY2EbZle6N8bXssZk8yK+gz6ReGr7k2lgHwIDAQABAoGANTb9im/MqV61yqRZknmkal1IRwdH3PR5SS4OW7qZbdp381st59VLAQrXowU84+ID2iksMftdMrbRpX7b/uAZZWQb6t1LhuLqo/GO7Ec0DwgQIYpNRDBi+UKqVsjpCbWuYO4rS6u/ezGLSSnD480XBqFBG8GexFg9nVKDfEC+QkECQQDjFieYLlXF8OjFaN2iOzF7M9JM4lGvvWgf9PcXq6MG4e3vcIVAqAr3+sHTuBFwgCOY0TtCPu3P+ndHEZoiREb/AkEAwwJn6O2rnE8/1y7/8BCHRs0AVrkBUmXKbGJUqwNUwLSs0ct0Q9b+jO22RBMTL6Q2q0zAhMiq9lyTlPp3uUMG4QJAd8A3IQlOMhPYrEGgnuyYK2AohmLSsNoxDqkfpFvenMKHtryfiHkVT6/shrmKeMkZ/HxZw5Vr00FBpTijD27yAQJAD4hNhnwHwzd8ukMAF7r5S2ZgtIef11RMPz3FyKlHQuIhMcSmyhCBze/RKLLsQEswH4zJeMoS8/SDvMYGazcvgQJBAKY9w6b2ilCFImZMdiqRJEVMu+eczsTEj9R1HF8Nk8X0hszAFLOqcUu9+MlFI/3C9tvP2P52rdXr/ckKRXP02IU=";
+        let  privkey = "-----BEGIN PRIVATE KEY-----\nMIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAKz8AoHDUYwcr7Ql\nLow/7pQnZYxRTXJfioqGQq+fW/NNCPMgR/Vf3b7iQfnXCStZWdbLx05tuUy7oIRE\nOz6WrOSaSnmndi/3Awec2HHBliv6oyvvdfk+SfjVCx1t76vEY7t1ZK6GZyBjYRtm\nV7o3xteyxmTzIr6DPpF4avuTaWAfAgMBAAECgYA1Nv2Kb8ypXrXKpFmSeaRqXUhH\nB0fc9HlJLg5buplt2nfzWy3n1UsBCtejBTzj4gPaKSwx+10yttGlftv+4BllZBvq\n3UuG4uqj8Y7sRzQPCBAhik1EMGL5QqpWyOkJta5g7itLq797MYtJKcPjzRcGoUEb\nwZ7EWD2dUoN8QL5CQQJBAOMWJ5guVcXw6MVo3aI7MXsz0kziUa+9aB/09xerowbh\n7e9whUCoCvf6wdO4EXCAI5jRO0I+7c/6d0cRmiJERv8CQQDDAmfo7aucTz/XLv/w\nEIdGzQBWuQFSZcpsYlSrA1TAtKzRy3RD1v6M7bZEExMvpDarTMCEyKr2XJOU+ne5\nQwbhAkB3wDchCU4yE9isQaCe7JgrYCiGYtKw2jEOqR+kW96cwoe2vJ+IeRVPr+yG\nuYp4yRn8fFnDlWvTQUGlOKMPbvIBAkAPiE2GfAfDN3y6QwAXuvlLZmC0h5/XVEw/\nPcXIqUdC4iExxKbKEIHN79EosuxASzAfjMl4yhLz9IO8xgZrNy+BAkEApj3DpvaK\nUIUiZkx2KpEkRUy755zOxMSP1HUcXw2TxfSGzMAUs6pxS734yUUj/cL228/Y/nat\n1ev9yQpFc/TYhQ==\n-----END PRIVATE KEY-----\n";
         
         
         let originString = "123456"
@@ -33,13 +32,13 @@ class LogDataMangerViewController: FZRequestViewController {
         // Demo: decrypt with private key
         decWithPrivKey = RSA.decryptString(encWithPubKey, privateKey: privkey)
          print("Decrypted with public key   " + decWithPrivKey)
+
         
-        
-//        let url = "http://fenzhi.wchao.org/api/user/login?phone=15910901725&password="+encWithPubKey
-//        Alamofire.request(url, method: .get).responseJSON { (returnResult) in
-//            print("secondMethod --> GET 请求 --> returnResult = \(returnResult)")
-//        }
-//        
+        let url = "http://fenzhi.wchao.org/api/user/login?client=iphone&version=071313&phone=15910901725&password="+encWithPubKey
+        Alamofire.request(url, method: .get).responseJSON { (returnResult) in
+            print("secondMethod --> GET 请求 --> returnResult = \(returnResult)")
+        }
+//
     }
     
     func register(phoneNum : String, paseWord : String, verification: String,completion : @escaping (_ data : Any) ->(), failure : @escaping (_ error : Any)->()) {

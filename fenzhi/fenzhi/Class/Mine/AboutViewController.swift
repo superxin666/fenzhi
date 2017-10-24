6105f93eac1b588463b7d7f2b79c142a64005dbf
lvxin <18730231789@163.com>
2017-10-24 18:13:13 +0800

    关于我们vc

---
+++ fenzhi/fenzhi/Class/Mine/AboutViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AboutViewController.swift
+//  fenzhi
+//
+//  Created by lvxin on 2017/10/24.
+//  Copyright © 2017年 Xunqiu. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

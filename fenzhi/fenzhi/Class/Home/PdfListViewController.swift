698b86acf27fe623711b730d290317a24cd7444c
lvxin <18730231789@163.com>
2017-12-17 11:26:23 +0800

    发布文件提示

--- fenzhi/fenzhi/Class/Home/PdfListViewController.swift
+++ fenzhi/fenzhi/Class/Home/PdfListViewController.swift
@@ -37,40 +37,45 @@ class PdfListViewController: BaseViewController {
         
         //第一部分
         let mutableStr = NSMutableAttributedString()
-        let str1 = NSAttributedString(string: "Ios选择添加文件发布需要先打开文件，例如，先在邮箱里打开要发布的文件，点击右上角")
+
+        let str1f = NSAttributedString(string: "Hi 您好！欢迎使用纷知。使用纷知iOS版上传文件，只需要以下几步：")
+        mutableStr.append(str1f)
+
+        let str1 = NSAttributedString(string: "\n1、打开需要上传的文件，例如在「QQ」里打开，先点击右上角")
         mutableStr.append(str1)
- 
+
         let attachment1 = NSTextAttachment()
         attachment1.image = #imageLiteral(resourceName: "icon_fx2-1")
         attachment1.bounds = CGRect(x: 0, y: 0, width: ip7(28), height: ip7(37))
         let attIcon1 = NSAttributedString(attachment: attachment1)
         mutableStr.append(attIcon1)
-        
+
+        let str1s = NSAttributedString(string: "按钮")
+        mutableStr.append(str1s)
         //第二部分
-        let str2 = NSAttributedString(string: "选择底部的“其他应用打开“")
+        let str2 = NSAttributedString(string: "\n2、选择「其他应用打开」。")
         mutableStr.append(str2)
+
+        
+        //第三部分
+        let str3 = NSAttributedString(string: "\n3、找到「拷贝至纷知")
+        mutableStr.append(str3)
         
         let attachment2 = NSTextAttachment()
-        attachment2.image = #imageLiteral(resourceName: "icon_qtyy-1")
+        attachment2.image = #imageLiteral(resourceName: "icon_kb-1")
         attachment2.bounds = CGRect(x: 0, y: 0, width: ip7(33), height: ip7(33))
         let attIcon2 = NSAttributedString(attachment: attachment2)
         mutableStr.append(attIcon2)
-        
-        
-        
-        //第三部分
-        let str3 = NSAttributedString(string: "，再选择“拷贝到纷知”拷贝到纷知")
-        mutableStr.append(str3)
 
-        let attachment3 = NSTextAttachment()
-        attachment3.image = #imageLiteral(resourceName: "icon_kb-1")
-        attachment3.bounds = CGRect(x: 0, y: 0, width: ip7(48), height: ip7(48))
-        let attIcon3 = NSAttributedString(attachment: attachment3)
-        mutableStr.append(attIcon3)
-        
-        let str4 = NSAttributedString(string: "\n建议：上官网发布带文件的分享更方便，官网：www.fenzhi-edu.com")
+        let str3s = NSAttributedString(string: "」并点选。")
+        mutableStr.append(str3s)
+
+        //第四部分
+        let str4 = NSAttributedString(string: "\n4、发布成功。")
         mutableStr.append(str4)
 
+        let str5 = NSAttributedString(string: "\n温馨提示：上官网发布带文件的分享更便捷，官网：www.fenzhi-edu.com")
+        mutableStr.append(str5)
         textView.attributedText = mutableStr
     
     }
